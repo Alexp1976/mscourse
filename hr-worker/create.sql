28e347b1e269f273de22b62217e1a367847aac94
Alexp1976 <rpdemo@gmail.com>
2020-11-04 19:34:24 -0300

    hr-user Mysql dev profile

--- hr-worker/create.sql
+++ hr-worker/create.sql
@@ -1,1 +1,2 @@
 create table tb_worker (id bigint not null auto_increment, daily_income double precision, name varchar(255), primary key (id)) engine=InnoDB
+create table tb_worker (id bigint not null auto_increment, daily_income double precision, name varchar(255), primary key (id)) engine=InnoDB
